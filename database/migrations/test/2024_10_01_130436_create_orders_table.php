6baeba929340895ecf94d008a48a604c25593d90
Neil <z5202dbc@gmail.com>
2024-10-01 21:38:53 +0800

    feat: Create tables: bnbs, rooms, and orders

---
+++ database/migrations/test/2024_10_01_130436_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('bnb_id');
+            $table->unsignedBigInteger('room_id');
+            $table->char('currency', 3);
+            $table->integer('amount');
+            $table->date('check_in_date');
+            $table->date('check_out_date');
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
